41b893f455e68cb9350c357a6322d28a02f57564
jaspervanzfive <jasperrecitas05@gmail.com>
2018-03-11 15:26:58 +0800

    Add files via upload

---
+++ CompleetKassa-master/CompleetKassa/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,68 @@
+﻿using CompleetKassa.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Interactivity;
+
+namespace CompleetKassa.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        private List<Category> categories = new List<Category>
+        {
+            new Category
+            {
+                Color = Colors.AliceBlue,
+                Name = "Socks"
+            },
+            new Category
+            {
+                Color = Colors.MediumVioletRed,
+                Name = "Milk"
+            },
+            new Category
+            {
+                Color = Colors.MediumSlateBlue,
+                Name = "Planes"
+            },
+              new Category
+            {
+                Color = Colors.AliceBlue,
+                Name = "Pants"
+            },
+            new Category
+            {
+                Color = Colors.MediumVioletRed,
+                Name = "Shoes"
+            },
+            new Category
+            {
+                Color = Colors.MediumSlateBlue,
+                Name = "Graphics"
+            },
+              new Category
+            {
+                Color = Colors.AliceBlue,
+                Name = "Keyboards"
+            },
+            new Category
+            {
+                Color = (Color)ColorConverter.ConvertFromString("#F48024"),
+                Name = "Shirts"
+            },
+            new Category
+            {
+                Color = Colors.MediumSlateBlue,
+                Name = "Shorts"
+            },
+        };
+        public ObservableViewModelCollection<CategoryViewModel, Category> CategoryViewModels { get; }
+        public MainWindowViewModel()
+        {
+            CategoryViewModels = new ObservableViewModelCollection<CategoryViewModel, Category>(categories, c => new CategoryViewModel(c));
+        }
+    }
+}
